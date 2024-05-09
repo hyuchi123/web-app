97c681fac1ce1d88ffa21b0d27a0e762e1f08fe1
hyuchi <hyuchi0922@gmail.com>
2024-05-09 20:41:00 +0800

    Finish ShopCategory

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -3,13 +3,14 @@ import { useContext } from 'react'
 import ',.CSS/ShopCategory.css'
 import { ShopContext } from '../Components/ShopContext'
 import Item from '../Components/Item/Item'
+//import deopdown_icon from '../Components/Assets/dropdown_icon.png
 
 
 export const ShopCategory = (props) => {
   const {all_product} = useContext(ShopContext);
   return (
     <div className="shop-category">
-      <img src={props.banner} alt="" />
+      <img className='shopcategory-banner' src={props.banner} alt="" />
       <div className="shopcategory-indexSort">
         <p>
           <span>Showing 1-12</span> out of 36 products
