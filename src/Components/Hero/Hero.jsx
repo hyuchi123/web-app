ef1d68a35217186342f66d88f4f7c4aca68165c6
yan0422de <153173336+yan0422de@users.noreply.github.com>
2024-05-15 17:34:59 +0800

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Hero.css'
-import hand_icon from '../Assets/hand_icon.png'
-import arrow_icon from '../Assets/arrow.png'
-import hero_image from '../Assets/hero_image.png'
+import home_pic from '../Assets/home_pic.png'
 
 export const Hero = () => {
   return (
     <div className='hero'>
-        <p>這裡應該要放一個童書網站首頁圖片</p>
+        <div className="home-pic">
+            <img src={home_pic} alt="" />
+        </div>
     </div>
   )
 }
