3de136029e2e3eb5b4ad720f45588fff6ea32be3
asdzzz000 <77500744+asdzzz000@users.noreply.github.com>
2024-05-20 14:09:15 +0800

    前後端串聯
    修改新書到貨、暢銷排行榜還有ShopContext的顯示

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Popular.css'
-import data_product from '../Assets/data'
 import { Item } from '../../Components/Item/Item';
 
 export const Popular = () => {
+  
+  const [popularProducts, setPopularProducts] = useState([])
+  
+  useEffect(()=>{
+    fetch('http://localhost:4000/popularproducts')
+    .then((res)=>res.json())
+    .then((data)=>setPopularProducts(data));
+  },[])
+
   return (
     <div className='Popular'>
         <h1>暢銷排行榜</h1>
         <hr/>
         <div className="popular-item">
-            {data_product.map((item,i)=>{
+            {popularProducts.map((item,i)=>{
                 return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
             })}
         </div>
