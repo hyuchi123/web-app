cde3d57f2911241b12846a8ec9aa360071a27ffb
kkkuo <kuofeini@gmail.com>
2024-05-29 17:45:52 +0800

    登入按鈕縮小、product display改了圖片大小、訊息呈現位置、按鈕顏色、新增顯示資訊、改登入頁面「登入按鈕」的長度

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -30,7 +30,11 @@ export const ProductDisplay = (props) => {
       <div className="product_info">
         <h1>{product.name}</h1>
         <div className="price">${product.new_price}</div>
-        
+        <div className="author">作者：{product.author}</div>
+        <div className="description">介紹：{product.description}</div>
+        <div className="age">適讀年齡：{product.age}</div>
+        <div className="language">語言：{product.language}</div>
+        <div className="topic">主題：{product.topic}</div>
       
       <label htmlFor="quantity">數量:</label>
         <select id="quantity" value={quantity} onChange={handleQuantityChange}>
@@ -49,70 +53,3 @@ export const ProductDisplay = (props) => {
     )
 }
 
-/*
-export const ProductDisplay = ({ product }) => {
-  const [quantity, setQuantity] = useState(1)
-  const [cart, setCart] = useState([])
-  const [isLoggedIn, setIsLoggedIn] = useState(false) // 新增登入狀態
-  const [message, setMessage] = useState('') // 新增訊息狀態
-
-
-  const handleQuantityChange = (event) => {
-    setQuantity(Number(event.target.value))
-  }
-
-  /*
-  //GreartStack version
-  const handleAddToCart = () => {
-    setCart([...cart, { ...product, quantity }])
-  }
-  */
- /*
-  const handleAddToCart = () => {
-    if (isLoggedIn) {
-      // 模擬將商品添加到購物車資料庫
-      addToCartDatabase({ ...product, quantity })
-      setMessage('商品已成功添加到購物車!')
-    } else {
-      setMessage('請先登入才能添加商品到購物車!')
-    }
-  }
-  // 模擬添加商品到購物車資料庫的函式
-  const addToCartDatabase = (productData) => {
-    // 在這裡可以實現將商品資訊發送到後端資料庫的邏輯
-    console.log('添加到購物車資料庫:')
-    console.log('商品id:', productData.id)
-    console.log('購買數量:', quantity)
-  }
-
-  return (
-    <div className='productdisplay'>
-      <div className='product_image'>
-        <img className='productdisplay_image' src={product.image} alt='' />
-      </div>
-
-      <div className="product_info">
-        <h1>{product.name}</h1>
-        <div className="price">${product.price}</div>
-        <p>{product.description}</p>
-      
-
-      
-      <label htmlFor="quantity">數量:</label>
-        <select id="quantity" value={quantity} onChange={handleQuantityChange}>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          {/* 下拉式選單可加更多選項 */
-/*        </select>
-      
-     
-        <button onClick={handleAddToCart}>加入購物車</button>
-        {message && <p>{message}</p>}
-      </div>
-        
-    </div>
-  )
-}*/
