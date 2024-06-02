a7888b9b5be94ca55e7ee05e371b2d06bafa608d
kkkuo <kuofeini@gmail.com>
2024-06-02 23:59:11 +0800

    把admin的的Shopper那個圖片換掉 登入者的圖片也換掉

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Navbar.css'
-import navlogo from '../../assets/nav-logo.svg'
-import navProfile from '../../assets/nav-profile.svg'
+import navlogo from '../../assets/nav-logo.png'
+import navProfile from '../../assets/user-cover.jpg'
 
 const Navbar = () => {
   return (
